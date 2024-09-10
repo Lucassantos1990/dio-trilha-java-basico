624ef28b5dc0ec9b23fd7a854ff91772beb9de81
Lucas Santos <lkweb.santos@gmail.com>
2024-09-10 12:04:18 -0300

    Atualização do curso

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+        
     }
 }
