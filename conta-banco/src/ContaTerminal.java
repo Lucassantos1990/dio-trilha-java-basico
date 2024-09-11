bc4d70b8610c7455cf0cbadaed990717d62a1cb8
Lucas Santos <lkweb.santos@gmail.com>
2024-09-11 12:44:36 -0300

    criação da sintaxe para conta-banco.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        System.out.println("Por favor Digite seu Nome: ");
+        String nome = scanner.next();
+
+        System.out.println(" Por favor Digite o Saldo à ser Depositado: ");
+        String saldo = scanner.next();
+
+        //Aqui o usuário recebe o numero da conta e agencia
+        int numero = 1021;
+
+        String agencia = "067-8";
+
+
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", sua conta " + numero + " e seu saldo " + saldo + ", já está disponível para saque.");
     }
 }
